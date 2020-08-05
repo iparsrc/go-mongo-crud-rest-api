b33f6af507a19ec83fa7e9bd78d91a4244483c91
parsa <parsaakbari1209@gmail.com>
2020-08-05 19:40:11 +0430

    Added CreateUser controller and url mapping.

--- app/mappings.go
+++ app/mappings.go
@@ -1,7 +1,11 @@
 package app
 
-import "github.com/parsaakbari1209/go-mongo-CRUD-web/controllers/ping"
+import (
+	"github.com/parsaakbari1209/go-mongo-CRUD-web/controllers/ping"
+	"github.com/parsaakbari1209/go-mongo-CRUD-web/controllers/users"
+)
 
 func MapUrls() {
 	router.GET("/ping", ping.Ping)
+	router.POST("/users/create", users.CreateUser)
 }
---
+++ controllers/users/users_controller.go
@@ -0,0 +1,25 @@
+package users
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/parsaakbari1209/go-mongo-CRUD-web/domain"
+	"github.com/parsaakbari1209/go-mongo-CRUD-web/services"
+	"github.com/parsaakbari1209/go-mongo-CRUD-web/utils"
+)
+
+func CreateUser(c *gin.Context) {
+	var newUser domain.User
+	if err := c.ShouldBindJSON(&newUser); err != nil {
+		restErr := utils.BadRequest("Invalid json.")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	user, restErr := services.CreateUser(&user)
+	if restErr != nil {
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	c.JSON(http.StatusCreated, user)
+}
