05bbbf3466252acf983d874a601a189f618e1c0f
parsa <parsaakbari1209@gmail.com>
2020-08-08 11:08:09 +0430

    Added /users/update endpoing.

--- app/mappings.go
+++ app/mappings.go
@@ -9,5 +9,6 @@ func MapUrls() {
 	router.GET("/ping", ping.Ping)
 	router.GET("/users/find", users.FindUser)
 	router.GET("users/delete", users.DeleteUser)
+	router.GET("/users/update", users.UpdateUser)
 	router.POST("/users/create", users.CreateUser)
 }
