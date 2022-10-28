6d99e786050af38fa26b8aeca857232c6d797703
Yuri Solyanik <jura.s.n@gmail.com>
2022-10-28 10:52:15 +0500

    Small fix in route

--- app/mappings.go
+++ app/mappings.go
@@ -8,7 +8,7 @@ import (
 func MapUrls() {
 	router.GET("/ping", ping.Ping)
 	router.GET("/users/find", users.FindUser)
-	router.GET("users/delete", users.DeleteUser)
+	router.GET("/users/delete", users.DeleteUser)
 	router.GET("/users/update", users.UpdateUser)
 	router.POST("/users/create", users.CreateUser)
 }
